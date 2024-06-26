2bb1b7df898053220204f53af6c47fdc50441e3f
Ramiro Gogol <gogolramiro@gmail.com>
2024-06-26 20:56:30 -0300

    get and post

--- apirestv2/src/main/java/com/uch/apirestv2/animal/AnimalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.uch.apirestv2.animal;
-
-import com.uch.apirestv2.animal.Animal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnimalRepository extends JpaRepository<Animal, Long> {
-}
--- apirestv2/src/main/java/com/uch/apirestv2/animal/HabitatRepository.java
+++
@@ -1,6 +0,0 @@
-package com.uch.apirestv2.animal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HabitatRepository extends JpaRepository<Habitat, Long> {
-}
